f20be3dec5549d334db251191f5fb7f77e0ab874
VIPIN KUMAR <kumar.vipin1189@gmail.com>
2022-01-30 10:33:43 +0530

    Build Checkout screen

--- frontend/src/components/ProductCart/ProductCart.actions.js
+++ frontend/src/components/ProductCart/ProductCart.actions.js
@@ -16,3 +16,12 @@ export const addToCart = (productId, qty) => async (dispatch, getState) => {
   });
   localStorage.setItem("cartItems", JSON.stringify(getState().cart.cartItems));
 };
+
+export const removeFromCart = (productId) => async (dispatch) => {
+  dispatch({
+    type: REMOVE_CART_ITEM,
+    payload: {
+      product: productId,
+    },
+  });
+}
